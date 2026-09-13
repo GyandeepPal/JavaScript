60e28e2ca231f4d9ade87fedfd7e050f760ee7f1
GyandeepPal <12345678+GyandeepPal@users.noreply.github.com>
2026-09-13 18:13:58 +0530

    index

---
+++ variable.js
@@ -0,0 +1,45 @@
+//////////////*************PREMETIVW DATA TYPE **************/////////////
+// var a=2;
+//  a=a+9;              // NOT ALLOWD
+// console.log(a+b);
+// var sr = "Gyan";
+// var s ='$';
+// console.log(typeof a, typeof b, typeof sr, typeof s);
+
+
+// let a =22;
+// a=a+1 ;                  //ALLOWD
+// {
+//     console.log(a);
+
+// }
+//     console.log(a);
+
+
+
+// const a =73;
+// a=a=66               // NOT ALLOWD
+// {
+// console.log(a);
+
+// }
+// console.log(a);
+
+
+///////////////////**************OBJECT DATA TYPE ///////////////**************
+
+
+// let x = 22;
+// let y = 33.44;
+// let z = "Gyan deep"
+// var a = undefined
+// const s = null;  NULL IS THE OBJECT
+// console.log(x, y, z, a, s);
+// console.log(typeof x, typeof y, typeof z, typeof a, typeof s);
+
+
+let d ={              //OBJECT
+    name :"gyan deep",    
+    "job code": 343323
+}
+console.log(d);
