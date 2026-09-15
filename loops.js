44ae25a85ef4a67eda53d46b904006a709ab5266
GyandeepPal <12345678+GyandeepPal@users.noreply.github.com>
2026-09-15 21:58:08 +0530

    Loops

---
+++ loops.js
@@ -0,0 +1,34 @@
+// let a = 0;
+// for (let i = 0; i <= 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "gayn",
+//     company: "gyan org",
+//     model: "Gyan deep AI"
+// }
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key ,element);
+
+// }
+
+// for (const c of "Gyanbhai") {
+//     console.log(c);
+    
+// }
+
+
+// let i=6;
+// while (i<7) {
+//     console.log(i);
+//     i++ 
+// }
+
+let i=0; 
+do{
+   console.log(i);
+    i++ 
+   
+} while(i<=10)
