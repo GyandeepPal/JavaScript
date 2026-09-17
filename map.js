3515963f0f182c382e1ad1e687c4e3164e0f565f
GyandeepPal <254293642+GyandeepPal@users.noreply.github.com>
2026-09-17 21:11:02 +0530

    filter

--- map.js
+++ map.js
@@ -1,8 +1,39 @@
-let arr =[1,2,3,4,5]
-let newarr =[]
-for (let index = 0; index < arr.length; index++) {
-    const element = arr[index];
-newarr.push(element**2)    
+// let arr =[1,2,3,4,5]
+// let newarr =[]
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+// newarr.push(element**2)    
+// }
+// console.log(newarr);
+
+
+//short form
+
+
+
+// let newarr=arr.map((e , index, array)=>{
+//     return e**3
+// })
+// console.log(newarr);
+
+//filter Array
+
+
+// let arr =[1,2,3,4,5]
+// let newarr=arr.filter((e)=>{
+//     return e<=3
+// })
+// console.log(newarr);
+
+
+let arr = [1, 2, 3, 45, 6, 6, 7,]
+
+const greaterThanSeven = (e) => {
+    if (e < 4) {
+        return true
+    }
+    false
 }
-console.log(newarr);
-// graph fix
+console.log(arr.filter(greaterThanSeven));
+
+
